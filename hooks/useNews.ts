1747e78636d632dacc5cadd82830b84e21d93fa1
Jonathan Mataloni <jo.mataloni@gmail.com>
2021-06-15 01:14:34 +0200

    Define API hooks

--- hooks/useNews.ts
+++ hooks/useNews.ts
@@ -1,30 +1,12 @@
-import { useEffect, useState } from "react";
 import { useQuery } from "react-query";
-
-import { REQ_STATUS } from "../interfaces";
-
 import { getNewsByCountryCode } from "../network/local";
 
 const QUERY_KEY = "news";
 
 const useNews = (countryCode: string) => {
-  const [status, setStatus] = useState<REQ_STATUS>(REQ_STATUS.LOADING);
-
-  const { isLoading, error, data } = useQuery(QUERY_KEY, () =>
+  return useQuery([QUERY_KEY, { countryCode }], () =>
     getNewsByCountryCode(countryCode)
   );
-
-  useEffect(() => {
-    if (isLoading) setStatus(REQ_STATUS.LOADING);
-    else if (error) setStatus(REQ_STATUS.ERROR);
-    else setStatus(REQ_STATUS.SUCCESS);
-  }, [isLoading, error, data]);
-
-  return {
-    status,
-    error,
-    data,
-  };
 };
 
 export default useNews;
